1af960c335890ae0e03a05d0184e8f6e82ff64d3
jose maldonado <150850527+relic465@users.noreply.github.com>
2024-12-16 12:21:26 +0100

    arregalndo contador magico

--- Home/Contador_boton_magico/action_main.js
+++ Home/Contador_boton_magico/action_main.js
@@ -3,14 +3,15 @@ let num = 0;
 
 function Sumar() {
     num = num + 1;
-    resultado.innerHTML = num + " gatitos";
+    resultado.innerHTML = num + " likes";
 }
 
 function Restar() {
     num = num - 1;
-    resultado.innerHTML = num + " gatitos";
+    resultado.innerHTML = num + " likes";
 }
 
 function Reset() {
     num = 0;
+    resultado.innerHTML = num + " likes";
 }
