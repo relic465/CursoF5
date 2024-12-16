23d04bde41932d72197bf1d9f754dda2c2e111e1
jose maldonado <150850527+relic465@users.noreply.github.com>
2024-12-16 13:30:14 +0100

    solucionando problemas compatibilidad de pantalla , mas array

--- Home/array/array.js
+++ Home/array/array.js
@@ -1,13 +1,22 @@
 let i = ["hola", "mundo"]; 
 let PK = ["charmander","bulbasaur","squirtle"];
-console.log(PK[0]);
 let saludo = ["hola" , "buenos", "dias"];
 let adios = document.querySelector("#bucle");
+let memes = ["https://cdn.pixabay.com/photo/2024/02/16/06/18/cat-8576777_1280.jpg" , "/images/meme(1).jpg" , "/images/meme(2).jpg" , "/images/meme(4).jpg"];
+
 
 function Coche(){
     PK.forEach(panda => {
         let parrafo = document.createElement("p");
         parrafo.textContent= panda;
         adios.appendChild(parrafo);
-    })
+    });
+}
+
+function Tap(){
+    memes.forEach(leon =>{
+        let imagen = document.createElement("img");
+        imagen.src= leon;
+        adios.appendChild(imagen);
+    });
 }
